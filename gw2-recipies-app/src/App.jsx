d9588239233fa51b5c4d3823fc4df7e78bd9e5d6
Kevin <kevin.velazquez.pz@gmail.com>
2025-08-05 12:12:11 -0400

    ReadMe changed, checkpoint-01

--- gw2-recipies-app/src/App.jsx
+++ gw2-recipies-app/src/App.jsx
@@ -5,6 +5,15 @@ function App() {
 
   return (
     <>
+      <div>
+      <h1>Hidersine's Junk Drawer</h1>
+      <p>A quick refence/look up tool for GW2 recipies</p>
+      </div>
+
+      <div>
+        <button type="text" placeholder="Search For items"/>
+      </div>
+
 
     </>
   )
---
+++ gw2-recipies-app/src/Search.jsx
@@ -0,0 +1,9 @@
+// take user input
+
+// comb the api
+
+// return an image of the item
+
+// return the recipie
+
+// return the cost for crafting
